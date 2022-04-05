bdb9fed9ec57e6872ca6a9c9d4b53d134aa8a89d
Brayan Iribe <brayani@certificaciondigital.mx>
2022-04-05 14:54:35 -0700

    Implementada ruta de status de microservicio

---
+++ server/src/main/java/minera/server/controller/StatusController.java
@@ -0,0 +1,22 @@
+package minera.server.controller;
+
+import java.util.HashMap;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/status")
+public class StatusController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<HashMap<String, Boolean>> status() {
+        HashMap<String, Boolean> response = new HashMap<>();
+        response.put("ok", true);
+        return new ResponseEntity<HashMap<String, Boolean>>(response, HttpStatus.OK);
+    }
+
+}
